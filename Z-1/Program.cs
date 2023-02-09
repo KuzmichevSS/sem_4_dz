ae25368b605d0ced490afcb7adde1069c4f6b131
Sergey Kuzmichev <kyz1979@mail.ru>
2023-02-09 22:35:39 +0300

    задача 1

---
+++ Z-1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A} в степени {B} = {Pow(A,B)}");
+
+
+int Pow(int num, int rank)
+{
+    if(B==0) return 1;
+
+    int result = num;
+    for (int i = 2; i <= rank; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
